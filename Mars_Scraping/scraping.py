2387efb5be32436df161966e27d22629cb5b7840
Collin LaVallee <clavallee2@gmail.com>
2021-11-27 21:35:42 -0600

    Challenge Updated

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -20,7 +20,7 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now(),
-        "hemis": scrape_hemis(browser)
+        "hemispheres": scrape_hemis(browser)
     }
 
     # Stop webdriver and return data
